9869c91e5d16236ed68b67d9a85adf445b7ce977
YuCao666 <psxyc18@nottingham.ac.uk>
2024-05-17 10:46:20 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "Heaton2016": {
+        "author": "Heaton, J. B. and Polson, N. G. and Witte, J. H.",
+        "journal": "Applied Stochastic Models in Business and Industry",
+        "keywords": "deep learning, finance, portfolio management",
+        "number": "1",
+        "pages": "3--12",
+        "title": "Deep learning for finance: deep portfolios",
+        "type": "article",
+        "url": "https://doi.org/10.1002/asmb.2209",
+        "volume": "33",
+        "year": "2016"
+    },
+    "Jacobs2021": {
+        "author": "Jacobs, G. and Hoste, V.",
+        "journal": "Language Resources and Evaluation",
+        "keywords": "information extraction, financial news, event detection, NLP",
+        "number": "1",
+        "pages": "225--257",
+        "title": "SENTiVENT: enabling supervised information extraction of company-specific events in economic and financial news",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s10579-021-09562-4",
+        "volume": "56",
+        "year": "2021"
+    },
+    "Obeidat2018": {
+        "author": "Obeidat, S. and Shapiro, D. and Lemay, M. and MacPherson, M. K. and Bolic, M.",
+        "journal": "International Journal on Advances in Intelligent Systems",
+        "keywords": "deep learning, portfolio management, asset allocation, adaptive systems",
+        "number": "1 & 2",
+        "title": "Adaptive Portfolio Asset Allocation Optimization with Deep Learning",
+        "type": "article",
+        "url": "https://personales.upv.es/thinkmind/dl/journals/intsys/intsys_v11_n12_2018/intsys_v11_n12_2018_3.pdf",
+        "volume": "11",
+        "year": "2018"
+    },
+    "Ozbayoglu2020": {
+        "author": "Ozbayoglu, A. M. and Gudelek, M. U. and Sezer, O. B.",
+        "journal": "Applied Soft Computing",
+        "keywords": "deep learning, financial applications, systematic review",
+        "pages": "106384",
+        "title": "Deep learning for financial applications\u202f: A survey",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.asoc.2020.106384",
+        "volume": "93",
+        "year": "2020"
+    },
+    "Park2022": {
+        "author": "Park, S. and Yang, J.",
+        "journal": "Knowledge-based Systems",
+        "keywords": "deep learning, LSTM, economic decision-making, interpretability",
+        "pages": "108907",
+        "title": "Interpretable deep learning LSTM model for intelligent economic decision-making",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.knosys.2022.108907",
+        "volume": "248",
+        "year": "2022"
+    },
+    "Sezer2020": {
+        "author": "Sezer, O. B. and Gudelek, M. U. and Ozbayoglu, A. M.",
+        "journal": "Applied Soft Computing",
+        "keywords": "deep learning, time series forecasting, financial markets",
+        "pages": "106181",
+        "title": "Financial time series forecasting with deep learning\u202f: A systematic literature review: 2005\u20132019",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.asoc.2020.106181",
+        "volume": "90",
+        "year": "2020"
+    },
+    "View2023": {
+        "keywords": "generative AI, financial robo-advisor, virtual advisors",
+        "title": "View of research on generative artificial intelligence for virtual Financial Robo-Advisor",
+        "type": "online",
+        "url": "https://drpress.org/ojs/index.php/ajst/article/view/19151/18713",
+        "year": "n.d."
+    },
+    "Wasserbacher2021": {
+        "author": "Wasserbacher, H. and Spindler, M.",
+        "journal": "Digital Finance",
+        "keywords": "machine learning, financial forecasting, financial planning",
+        "number": "1",
+        "pages": "63--88",
+        "title": "Machine learning for financial forecasting, planning and analysis: recent developments and pitfalls",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s42521-021-00046-2",
+        "volume": "4",
+        "year": "2021"
+    },
+    "Xing2017": {
+        "author": "Xing, F. Z. and Cambria, E. and Welsch, R. E.",
+        "journal": "Artificial Intelligence Review",
+        "keywords": "NLP, financial forecasting, AI in finance",
+        "number": "1",
+        "pages": "49--73",
+        "title": "Natural language based financial forecasting: a survey",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s10462-017-9588-9",
+        "volume": "50",
+        "year": "2017"
+    },
+    "Xu2019": {
+        "author": "Xu, J. and Guo, L. and Jiang, J. and Ge, B. and Li, M.",
+        "journal": "Technological Forecasting and Social Change",
+        "keywords": "deep learning, automatic extraction, technical intelligence",
+        "pages": "339--351",
+        "title": "A deep learning methodology for automatic extraction and discovery of technical intelligence",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.techfore.2019.06.004",
+        "volume": "146",
+        "year": "2019"
+    }
+}});
